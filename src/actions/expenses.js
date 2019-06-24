ddb17273d4b4b437dbfb944b79898df493cd3287
Amit Kumar Singh <amit.ks30999@gmail.com>
2019-06-24 14:16:02 +0530

    Setting Herko development variables

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,25 +1,50 @@
 import uuid from 'uuid'
 //ADD EXPENSE
 
-//destruction 
-export const addExpense = ({
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
-} = {}
 
-) => ({
+//components calls action generator
+// action generator return object
+// component dispatches object
+// redux store changes
+
+
+//With firebase
+
+//components calls action generator
+// action generator return function
+// component dispaches function
+// function runs (has the ability to dispach other actions and do whatever it wants)
+
+import database from '../firebase/firebase'
+//destruction 
+export const addExpense = (expense) => ({
 
 type: 'ADD_EXPENSE',
-expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-}
+expense
 })
+
+export const startAddExpense=(expenseDate={})=>{
+    return (dispatch)=>{
+
+        const {
+         
+            description='',
+            note='',
+            amount=0,
+            createdAt=0
+        } = expenseDate;
+
+        const expense={description,note,amount,createdAt}
+        database.ref('expenses').push(expense).then((ref)=>{
+            //dispatch action to redux store
+
+            dispatch(addExpense({
+                id:ref.key,
+                ...expense
+            }));
+        })
+    }
+}
 //REMOVE EXPENSE
 
 export const removeExpense = ({
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,55 +1,20 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDhpLQVSxdzdVytcY_02G_d9yh4ZcFE_qA",
-    authDomain: "expensify-768b6.firebaseapp.com",
-    databaseURL: "https://expensify-768b6.firebaseio.com",
-    projectId: "expensify-768b6",
-    storageBucket: "expensify-768b6.appspot.com",
-    messagingSenderId: "458383824312",
-    appId: "1:458383824312:web:5e57250fa873e999"
+    apiKey: process.env.FIREBASE_API_KEY  ,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
  
    // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
    const database=firebase.database()
 
-   //Fetching data
-
-//  1.
-// database.ref()
-// .once('value')
-// .then((snapshot)=>{
-
-//     const val=snapshot.val();
-//     console.log(val);
-    
-
-// }).catch((e)=>{
-//     console.log("Error fetching data",e);
-    
-// })
-
-
-
-// const onValueChange=database.ref().on('value',(snapshot)=>{
-
-//     console.log(snapshot.val());
-    
-// },(e)=>{
-//     console.log("Error in fetching data",e);
-    
-// })
-
-//To unsubscribe to on
-//database.ref().off(onValueChange)
-
-
-
-
-  //.set() return a promise
-
-//   database.ref().set({
+//      database.ref().set({
 //       name:"Amit",
 //       age:20,
 //       isSingle:true,
@@ -66,33 +31,4 @@ const firebaseConfig = {
 //   })
 
 
-  //Removing data from database
-
-//  1.database.ref().set(null)
-
-// 2. database.ref().remove().then(()=>{
-//     console.log("Success");
-    
-// }).catch((e)=>{
-
-//     console.log("error :" ,e);
-    
-// })
-
-
-// Updating data
-
-
-// database.ref().update({
-//     "location/city":"Model Town"
-// })
-
-
-
-database.ref().on('value',(snapshot)=>{
-
-    const name=snapshot.val().name;
-    const city=snapshot.val().location.city;
-    console.log(`${name} lives in ${city} `);
-    
-})
+export { database as default}
